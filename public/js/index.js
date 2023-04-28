d3b8f08f46244281403535899b8172c3391cb415
riad40 <riadabdelaziz40@gmail.com>
2023-04-28 15:26:30 +0000

    - make the Calculator class that will handle all the logic of the app - make numbers buttons functional ( make the appendNumber and updateDisplay methods )

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("init Project");
+class Calculator {
+    // Methods
+    constructor(previusNumberDisplayer, currentNumberDisplayer) {
+        // Properties
+        this.prevNumber = "";
+        this.currentNumber = "";
+        this.operator = "";
+        this.operatorClicked = false;
+        this.prevNumberDisplayer = previusNumberDisplayer;
+        this.currentNumberDisplayer = currentNumberDisplayer;
+    }
+    clear() {
+        this.currentNumber = "";
+        this.prevNumber = "";
+        this.operator = "";
+        this.operatorClicked = false;
+    }
+    appendNumber(number) {
+        console.log("sjdhsjfsjdgfj");
+        if (number === "." && this.currentNumber.includes("."))
+            return;
+        this.currentNumber = this.currentNumber + number;
+    }
+    updateDisplay() {
+        this.currentNumberDisplayer.innerText = this.currentNumber;
+        this.prevNumberDisplayer.innerText = this.prevNumber;
+    }
+}
+// grab the DOM elements
+const clear = document.querySelector("#clear");
+const operators = document.querySelectorAll("#operator");
+const numbers = document.querySelectorAll("#number");
+const equal = document.querySelector("#equal");
+const previousNumberDisplayer = document.querySelector(".previousNumber");
+const currentNumberDisplayer = document.querySelector(".currentNumber");
+// create a new instance of the Calculator class
+const calculator = new Calculator(previousNumberDisplayer, currentNumberDisplayer);
+// add event listeners to the DOM elements
+// numbers
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        calculator.appendNumber(number.innerText);
+        calculator.updateDisplay();
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-console.log("init Project")
+class Calculator {
+    // Properties
+
+    public prevNumber: string = ""
+    public currentNumber: string = ""
+    public operator: string = ""
+    public operatorClicked: boolean = false
+    public prevNumberDisplayer: HTMLDivElement
+    public currentNumberDisplayer: HTMLDivElement
+
+    // Methods
+    constructor(previusNumberDisplayer: HTMLDivElement, currentNumberDisplayer: HTMLDivElement) {
+        this.prevNumberDisplayer = previusNumberDisplayer
+        this.currentNumberDisplayer = currentNumberDisplayer
+    }
+
+    clear() {
+        this.currentNumber = ""
+        this.prevNumber = ""
+        this.operator = ""
+        this.operatorClicked = false
+    }
+
+    appendNumber(number: string) {
+        console.log("sjdhsjfsjdgfj")
+        if (number === "." && this.currentNumber.includes(".")) return
+        this.currentNumber = this.currentNumber + number
+    }
+
+    updateDisplay() {
+        this.currentNumberDisplayer.innerText = this.currentNumber
+        this.prevNumberDisplayer.innerText = this.prevNumber
+    }
+}
+
+// grab the DOM elements
+const clear = document.querySelector("#clear") as HTMLButtonElement
+const operators = document.querySelectorAll("#operator") as NodeListOf<HTMLButtonElement>
+const numbers = document.querySelectorAll("#number") as NodeListOf<HTMLButtonElement>
+const equal = document.querySelector("#equal") as HTMLButtonElement
+const previousNumberDisplayer = document.querySelector(".previousNumber") as HTMLDivElement
+const currentNumberDisplayer = document.querySelector(".currentNumber") as HTMLDivElement
+
+// create a new instance of the Calculator class
+const calculator = new Calculator(previousNumberDisplayer, currentNumberDisplayer)
+
+// add event listeners to the DOM elements
+
+// numbers
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        calculator.appendNumber(number.innerText)
+        calculator.updateDisplay()
+    })
+})
